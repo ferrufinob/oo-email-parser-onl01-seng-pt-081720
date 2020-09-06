21f34109e75331740192d5c0b8ae6678b16fc197
Brenda Ferrufino <ferrufinob@gmail.com>
2020-09-06 17:30:36 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,5 +4,19 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-
+class Parser
+ 3 
+ 5     def self.parse(data)
+ 7     # Pass the data over to the parser instance
+ 8     tree = @@parser.parse(data)
+ 9     
+10     # If the AST is nil then there was an error during parsing
+11     # we need to report a simple error message to help the user
+12     if(tree.nil?)
+13       raise Exception, "Parse error at offset: #{@@parser.index}"
+14     end
+15     
+16     return tree
+17   end
+18 end
 end
